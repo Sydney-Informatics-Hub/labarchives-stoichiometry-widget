b75f3501ddcb3e8db2ea73986d90897d97b5fbf1
vijay <vijay.raghunath@sydney.edu.au>
2019-03-25 10:33:11 +1100

    check existing behaviour

--- script.js
+++ script.js
@@ -27,127 +27,149 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 my_widget_script =
-{
-  init:function (mode, json_data) {
-    //this method is called when the form is being constructed
-    // parameters
-    // mode = if it equals 'view' than it should not be editable
-    //        if it equals 'edit' then it will be used for entry
-    //        if it equals 'view_dev' same as view,  does some additional checks that may slow things down in production
-    //        if it equals 'edit_dev' same as edit,   does some additional checks that may slow things down in production
- 
-    // json_data will contain the data to populate the form with, it will be in the form of the data
-    // returned from a call to to_json or empty if this is a new form.
-    //By default it calls the parent_class's init.
- 
-    //TO DO write code specific to your form
-    this.parent_class.init(mode, json_data);
-    
-    if (mode.indexOf('view') > -1) {
-      var isEmpty = function(tr) {
-        var inputs = $('input', tr);
-        for (var i = 0; i < inputs.length; i++) {
-          if ($(inputs[i]).val()) {
-            return false;
+    {
+      init:function (mode, json_data) {
+        //this method is called when the form is being constructed
+        // parameters
+        // mode = if it equals 'view' than it should not be editable
+        //        if it equals 'edit' then it will be used for entry
+        //        if it equals 'view_dev' same as view,  does some additional checks that may slow things down in production
+        //        if it equals 'edit_dev' same as edit,   does some additional checks that may slow things down in production
+
+        // json_data will contain the data to populate the form with, it will be in the form of the data
+        // returned from a call to to_json or empty if this is a new form.
+        //By default it calls the parent_class's init.
+
+        //TO DO write code specific to your form
+        console.log('Welcome to stoichiometry world today!')
+        console.log('Hello World  - q')
+
+        this.parent_class.init(mode, json_data);
+
+        if (mode.indexOf('view') > -1) {
+          var isEmpty = function(tr) {
+            var inputs = $('input', tr);
+            for (var i = 0; i < inputs.length; i++) {
+              if ($(inputs[i]).val()) {
+                return false;
+              }
+            }
+            return true;
           }
+
+          $('#the_form tbody tr').each(function() {
+            if (isEmpty(this)) {
+              $(this).remove();
+            }
+          })
+          return;
         }
-        return true;
-      }
-     
-      $('#the_form tbody tr').each(function() {
-          if (isEmpty(this)) {
-            $(this).remove();
+
+        var nFixed = 2;
+
+        $('#the_form input[name^=amount]').on('keyup change', function() {
+          console.log('Key up change on Amount cell')
+          var tr = $(this).closest('tr');
+          var equiv = $('input[name^=equivalents]', tr);
+          if (equiv.length != 1)
+            throw ('Found the incorrect number of equiv inputs: ' + equiv.length)
+          equiv = equiv.first();
+          var moles1 = $('#the_form input[name=moles1_formula]');
+          var fw = $('input[name^=fw]', tr);
+          if ($(this).val() && fw.val() && moles1.val())
+            equiv.val(($(this).val() / fw.val() / moles1.val()).toFixed(nFixed))
+        });
+
+        $('#the_form input[name^=fw]').on('keyup change', function() {
+          console.log('Key up change on FW cell')
+          var tr = $(this).closest('tr');
+          var amount = $('input[name^=amount]', tr);
+          var fw = $('input[name^=fw]', tr);
+          var moles = $('input[name^=moles]', tr);
+          var equiv = $('input[name^=equivalents]', tr);
+          if (moles.val() && fw.val()) {
+            amount.val((moles.val() * fw.val()).toFixed(nFixed))
           }
-        })
-      return;
+        });
+
+        $('#the_form input[name^=fw]').on('change', function() {
+          console.log('Key up change on FW cell')
+          var tr = $(this).closest('tr');
+          var amount = $('input[name^=amount]', tr);
+          var equiv = $('input[name^=equivalents]', tr);
+          if (!equiv.val()) {
+            amount.change();
+          }
+        });
+
+        $('#the_form input[name^=equivalents]').on('keyup change', function() {
+          var tr = $(this).closest('tr');
+          var amount = $('input[name^=amount]', tr);
+          var fw = $('input[name^=fw]', tr);
+          var moles = $('input[name^=moles]', tr);
+          var moles1 = $('#the_form input[name=moles1_formula]');
+          var equiv = $('input[name^=equivalents]', tr);
+          if (equiv.val() && fw.val() && moles1.val()) {
+            moles.val((moles1.val() * equiv.val()).toFixed(nFixed));
+            fw.change();
+          }
+        });
+
+        $('#the_form input[name^=equivalents]').on('keyup change', function() {
+
+        });
+
+        document.getElementById('my_cell').onchange = function () {
+          console.log('reached some where')
+        }
+
+
+      },
+
+      to_json:function () {
+        //should return a json string containing the data entered into the form by the user
+        //whatever is return from the method is persisted in LabArchives.  must not be binary data.
+        //called when the user hits the save button, when adding or editing an entry
+
+
+        //TO DO write code specific to your form
+        return this.parent_class.to_json();
+      },
+
+      from_json:function (json_data) {
+        //populates the form with json_data
+        //TO DO write code specific to your form
+        this.parent_class.from_json(json_data);
+      },
+
+      test_data:function () {
+        //during development this method is called to populate your form while in preview mode
+        //TO DO write code specific to your form
+        return this.parent_class.test_data();
+      },
+
+      is_valid:function (b_suppress_message) {
+        //called when the user hits the save button, to allow for form validation.
+        //returns an array of dom elements that are not valid - default is those elements marked as mandatory
+        // that have no data in them.
+        //You can modify this method, to highlight bad form elements etc...
+        //LA calls this method with b_suppress_message and relies on your code to communicate issues to the user
+        //Returning an empty array [] or NULL equals no error
+        //TO DO write code specific to your form
+
+        return this.parent_class.is_valid(b_suppress_message);
+      },
+
+      is_edited:function () {
+        //should return true if the form has been edited since it was loaded or since reset_edited was called
+        return this.parent_class.is_edited();
+      },
+
+      reset_edited:function () {
+        //typically called have a save
+        //TO DO write code specific to your form
+        return this.parent_class.reset_edited();
+      }
+
+
     }
-   
-    var nFixed = 2;
-    $('#the_form input[name^=amount]').on('keyup change', function() {
-      var tr = $(this).closest('tr');
-      var equiv = $('input[name^=equivalents]', tr);
-      if (equiv.length != 1)
-        throw ('Found the incorrect number of equiv inputs: ' + equiv.length)
-      equiv = equiv.first();
-      var moles1 = $('#the_form input[name=moles1_formula]');
-      var fw = $('input[name^=fw]', tr);
-      if ($(this).val() && fw.val() && moles1.val())
-        equiv.val(($(this).val() / fw.val() / moles1.val()).toFixed(nFixed))
-    });
-    $('#the_form input[name^=fw]').on('keyup change', function() {
-      var tr = $(this).closest('tr');
-      var amount = $('input[name^=amount]', tr);
-      var fw = $('input[name^=fw]', tr);
-      var moles = $('input[name^=moles]', tr);
-      var equiv = $('input[name^=equivalents]', tr);
-      if (moles.val() && fw.val()) {
-        amount.val((moles.val() * fw.val()).toFixed(nFixed))
-      }
-    });
-    $('#the_form input[name^=fw]').on('change', function() {
-      var tr = $(this).closest('tr');
-      var amount = $('input[name^=amount]', tr);
-      var equiv = $('input[name^=equivalents]', tr);
-      if (!equiv.val()) {
-        amount.change();
-      }
-    });
-    $('#the_form input[name^=equivalents]').on('keyup change', function() {
-      var tr = $(this).closest('tr');
-      var amount = $('input[name^=amount]', tr);
-      var fw = $('input[name^=fw]', tr);
-      var moles = $('input[name^=moles]', tr);
-      var moles1 = $('#the_form input[name=moles1_formula]');
-      var equiv = $('input[name^=equivalents]', tr);
-      if (equiv.val() && fw.val() && moles1.val()) {
-        moles.val((moles1.val() * equiv.val()).toFixed(nFixed));
-        fw.change();
-      }
-    });
-  },
- 
-  to_json:function () {
-    //should return a json string containing the data entered into the form by the user
-    //whatever is return from the method is persisted in LabArchives.  must not be binary data.
-    //called when the user hits the save button, when adding or editing an entry
- 
- 
-    //TO DO write code specific to your form
-    return this.parent_class.to_json();
-  },
- 
-  from_json:function (json_data) {
-    //populates the form with json_data
-    //TO DO write code specific to your form
-    this.parent_class.from_json(json_data);
-  },
- 
-  test_data:function () {
-    //during development this method is called to populate your form while in preview mode
-    //TO DO write code specific to your form
-    return this.parent_class.test_data();
-  },
- 
-  is_valid:function (b_suppress_message) {
-    //called when the user hits the save button, to allow for form validation.
-    //returns an array of dom elements that are not valid - default is those elements marked as mandatory
-    // that have no data in them.
-    //You can modify this method, to highlight bad form elements etc...
-    //LA calls this method with b_suppress_message and relies on your code to communicate issues to the user
-    //Returning an empty array [] or NULL equals no error
-    //TO DO write code specific to your form
- 
-    return this.parent_class.is_valid(b_suppress_message);
-  },
- 
-  is_edited:function () {
-    //should return true if the form has been edited since it was loaded or since reset_edited was called
-    return this.parent_class.is_edited();
-  },
- 
-  reset_edited:function () {
-    //typically called have a save
-    //TO DO write code specific to your form
-    return this.parent_class.reset_edited();
-  }
-}
