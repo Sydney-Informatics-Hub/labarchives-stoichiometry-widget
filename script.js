7bbb01c9b3bf3a90936c0787497962078e767c49
vijay <vijay.raghunath@sydney.edu.au>
2019-04-12 13:15:30 +1000

    removed a redundancy in fw change handler

--- script.js
+++ script.js
@@ -108,15 +108,15 @@ my_widget_script =
           var moles = $('input[name^=moles]', tr);
           if (is_initial_row) {
             if (amount.val() && moles.val()) {
-              amount.val((moles.val() * fw.val()).toFixed(nFixed))
-            } else if (amount.val() && !moles.val()) {
-              moles.val((amount.val() / fw.val()).toFixed(nFixed))
-            } else if (moles.val() && !amount.val()) {
-              amount.val((moles.val() * fw.val()).toFixed(nFixed))
-            }
-            if (amount.val() && moles.val()) {
-              my_widget_script.enable_all_records()
-            }
+                amount.val((moles.val() * fw.val()).toFixed(nFixed))
+                my_widget_script.enable_all_records()
+              } else if (amount.val()) {
+                moles.val((amount.val() / fw.val()).toFixed(nFixed))
+                my_widget_script.enable_all_records()
+              } else if (moles.val()) {
+                amount.val((moles.val() * fw.val()).toFixed(nFixed))
+                my_widget_script.enable_all_records()
+              }
           } else {
             var equiv = $('input[name^=equivalents]', tr);
             var moles1 = $('#the_form input[name=moles1_number]');
